dd126a8a55f96d4338b52c09f251841b830cdeb4
Michael Buluma <buluma@users.noreply.github.com>
2022-01-21 09:15:30 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ mkdir -p $USER_HOME
 groupadd -g $GROUP_ID -o $GROUP_NAME
 useradd --shell /bin/bash -u $USER_ID -o -c "" -g $GROUP_NAME -M -d $USER_HOME $USER_NAME
 chown $USER_NAME:$GROUP_NAME $USER_HOME
-exec /usr/local/bin/gosu $USER_NAME "$@"
+# exec /usr/local/bin/gosu $USER_NAME "$@"
